00c22187f5cd78d3ae6f89ab73da011c0a9f09b4
adricastro1 <adricastro1196@gmail.com>
2023-03-07 10:56:07 -0500

    randomWord and word split done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 /*----- constants -----*/
 const wordChoices = {
-    comet: [c, o, m, e, t],
-    orbit: [o, r, b, i, t],
-    pluto: [p, l, u, t, o],
-}
+    comet: ['c', 'o', 'm', 'e', 't'],
+    orbit: ['o', 'r', 'b', 'i', 't'],
+    pluto: ['p', 'l', 'u', 't', 'o'],
+};
 
-// alphabet as buttons?
 
 /*----- state variables -----*/
 let currentWord;
 let guessNum;
 let letterGuess;
-
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('#message');
 const playAgainBtn = document.querySelector('button');
-const correctLetter = document.querySelector('#word-spaces');
+const correctLetter = document.querySelector('#letter-spaces');
 
 /*----- event listeners -----*/
 // keyboard event listener
@@ -27,22 +25,37 @@ init()
 
 // initialize function
 function init() {
+    letterSpaces = [null, null, null, null, null];
+    currentWord = null;
+    guessNum = 0;
     render()
 }
 
 function render() {
     renderGame();
-    renderMessage();
+    // renderMessage();
 }
 
 function renderGame() {
     // pick random word from wordChoices 
-    // 
+    const currentWord = chooseWord()
+    const letters = currentWord.split('')
+    console.log(letters)
+}
+
+function chooseWord() {
+    const options = ['comet', 'orbit', 'pluto']
+    const randomWord = Math.floor(Math.random() * options.length)
+    return options[randomWord]
+}
+
+function renderWord(){
+
 }
 
 // function for amount of guesses left?
 
-function renderMessage() {
+// function renderMessage() {
     // if full word is guessed before 8 guesses render 'you win!'
     // if != full word and 8 max guesses render "try again!"
-}
+// }
