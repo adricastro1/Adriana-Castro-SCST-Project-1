6e65e6ce9723c52fc9f62427969e4dd6a313fd05
adricastro1 <adricastro1196@gmail.com>
2023-03-07 14:24:11 -0500

    render word to board

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 /*----- constants -----*/
-const wordChoices = {
-    comet: ['c', 'o', 'm', 'e', 't'],
-    orbit: ['o', 'r', 'b', 'i', 't'],
-    pluto: ['p', 'l', 'u', 't', 'o'],
-};
-
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 /*----- state variables -----*/
 let currentWord;
 let guessNum;
 let letterGuess;
+let keyName;
+
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('#message');
 const playAgainBtn = document.querySelector('button');
-const correctLetter = document.querySelector('#letter-spaces');
+const correctLetter = document.querySelector('#letter-spaces>div');
 
 /*----- event listeners -----*/
 // keyboard event listener
 
+document.body.addEventListener("keyup", keyLetter);
+
 // play again button
+playAgainBtn.addEventListener('click', init)
 
 /*----- functions -----*/
 init()
@@ -33,15 +35,24 @@ function init() {
 
 function render() {
     renderGame();
-    // renderMessage();
+    renderMessage();
+    // renderWord();
 }
 
-function renderGame() {
+function renderGame(evt) {
     // pick random word from wordChoices 
     const currentWord = chooseWord()
-    const letters = currentWord.split('')
-    console.log(letters)
+    const lettersArray = currentWord.split('')
+    console.log(lettersArray)
+    lettersArray.forEach(function (letter, idx) {
+        const letterId = `${idx}`
+        const letterEl = document.getElementById(letterId)
+        letterEl.innerText = [letter]
+        console.log(letterId)
+    })
+
 }
+
 
 function chooseWord() {
     const options = ['comet', 'orbit', 'pluto']
@@ -49,13 +60,22 @@ function chooseWord() {
     return options[randomWord]
 }
 
-function renderWord(){
+function keyLetter(evt) {
+    const keyName = evt.key;
+    let guess = keyName
+
+}
+
+function renderWord() {
 
 }
 
 // function for amount of guesses left?
 
-// function renderMessage() {
+function renderMessage() {
     // if full word is guessed before 8 guesses render 'you win!'
     // if != full word and 8 max guesses render "try again!"
-// }
+    if (keyLetter) {
+        messageEl.innerText = "keep trying!"
+    }
+}
